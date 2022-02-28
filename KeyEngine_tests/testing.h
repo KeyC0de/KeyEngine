23acc9218a4e065bb238eb6c37e28f7f3079572b
KeyC0de <nik.lazkey@gmail.com>
2022-02-28 23:04:07 +0200

    reorganized projects

---
+++ KeyEngine_tests/testing.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+void testDynamicConstant();
+void testDynamicMeshLoading();
+void testMaterialSystemLoading( class Graphics& gph );
+void testScaleMatrixTranslation();
+void testDynamicVertex( class Window& wnd );
+void testNumpy();
