5c67b6791ff968cf5149e1ae93c3591201e06eda
KeyC0de <nik.lazkey@gmail.com>
2022-09-17 20:30:04 +0300

    reworking testing

--- KeyEngine/inc/testing.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-void testDynamicConstant();
-void testDynamicMeshLoading();
-void testMaterialSystemLoading( class Graphics &gph );
-void testScaleMatrixTranslation();
-void testDynamicVertex( class Window &wnd );
-void testNumpy();
